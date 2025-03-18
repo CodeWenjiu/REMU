bbd1ffc439d893f84ee32c8006a473080c20fd5f
wenjiu <2784307979@qq.com>
2025-03-18 11:12:30 +0800

    feat: better Error report

--- core/src/main.rs
+++ core/src/main.rs
@@ -1,25 +1,35 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, path::PathBuf};
 
 use config::Config;
+use owo_colors::OwoColorize;
+use snafu::ResultExt;
 
-fn main() {
+#[derive(Debug, snafu::Snafu)]
+enum PanicError {
+    #[snafu(display("{} {}: {}", "Unable to parse config file from".red(), path.display(), source))]
+    ConfigParse { source: config::ConfigError, path: PathBuf },
+
+    #[snafu(display("{}: {}", "Unable to deserilalize file from".red(), source))]
+    ConfigDeserialize { source: config::ConfigError }
+}
+
+type Result<T, E = PanicError> = std::result::Result<T, E>;
+
+#[snafu::report]
+fn main() -> Result<()> {
+    let path = "config/config";
+
     let settings = Config::builder()
         // Add in `./Settings.toml`
-        .add_source(config::File::with_name("config/config"))
+        .add_source(config::File::with_name(path))
         .add_source(config::Environment::with_prefix(""))
         .build()
-        .unwrap();
+        .context(ConfigParseSnafu { path })?;
 
-    // println!(
-    //     "{:?}",
-    //     settings
-    //         .try_deserialize::<HashMap<String, String>>()
-    //         .unwrap()
-    // );
-    // print iterator
-
-    let map = settings.try_deserialize::<HashMap<String, String>>().unwrap();
+    let map = settings.try_deserialize::<HashMap<String, String>>().context(ConfigDeserializeSnafu)?;
     for (key, value) in map.iter().filter(|(k, _)| k.as_str() != "") {
         println!("{}: {}", key, value);
     }
+
+    Ok(())
 }
