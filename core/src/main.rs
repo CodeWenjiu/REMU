0358bb546f100a4c78cc26070bc870f1b0d0397f
wenjiu <2784307979@qq.com>
2025-03-18 08:47:02 +0800

    fix: correct parse path

--- core/src/main.rs
+++ core/src/main.rs
@@ -37,9 +37,7 @@ use config::Config;
 fn main() {
     let settings = Config::builder()
         // Add in `./Settings.toml`
-        .add_source(config::File::with_name("../../config/.config.json"))
-        // Add in settings from the environment (with a prefix of APP)
-        // Eg.. `APP_DEBUG=1 ./target/app` would set the `debug` key
+        .add_source(config::File::with_name("config/config"))
         .add_source(config::Environment::with_prefix(""))
         .build()
         .unwrap();
