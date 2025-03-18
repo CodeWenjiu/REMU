f5af7d531cd1451b845ca12c055df233aea64ccd
wenjiu <2784307979@qq.com>
2025-03-18 16:57:41 +0800

    feat: logger

--- core/src/main.rs
+++ core/src/main.rs
@@ -1,8 +1,23 @@
 use option_parser::parse;
 use simple_debugger::SimpleDebugger;
+use tracing::error;
+use tracing_appender::{non_blocking, rolling};
+use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, Registry};
 
 fn main() -> Result<(), ()> {
     let cli_result = parse()?;
+
+    let file_appender = rolling::never("logs", ".log");
+    let (non_blocking_appender, _guard) = non_blocking(file_appender);
+    let file_layer = fmt::layer()
+        .with_ansi(false)
+        .with_writer(non_blocking_appender);
+        
+    Registry::default()
+        .with(file_layer)
+        .init();
+
+    color_eyre::install().map_err(|e| error!("Unable to install color_eyre: {}", e))?;
 
     let debugger = SimpleDebugger::new(cli_result);
     debugger.mainloop()?;
