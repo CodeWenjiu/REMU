2970d9b9be0218c37cb049bdfba9bff07055f593
wenjiu <2784307979@qq.com>
2025-03-22 11:16:21 +0800

    chore(wip): change work place
    Signed-off-by: wenjiu <2784307979@qq.com>

--- simulator/src/emu/emu.rs
+++ simulator/src/emu/emu.rs
@@ -1,17 +1,24 @@
 use crate::Simulator;
 
 pub struct Emu {
-    
+    rv32i_ena: bool,
+    rv32m_ena: bool,
+    rv32e_ena: bool,
+    zicsr_ena: bool,
+    priv_ena: bool,
 }
 
 impl Simulator for Emu {
-
 }
 
 impl Emu {
     pub fn new() -> Self {
         Self {
-            
+            rv32i_ena: true,
+            rv32m_ena: true,
+            rv32e_ena: true,
+            zicsr_ena: true,
+            priv_ena: true,
         }
     }
 }
--- simulator/src/emu/isa/mod.rs
+++ simulator/src/emu/isa/mod.rs
@@ -1,6 +1,1 @@
 remu_macro::mod_pub!(riscv);
-
-pub struct Pattern {
-    pub mask: u32,
-    pub value: u32,
-}
