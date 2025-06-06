29ba2ea7693df3da33dd84c3c4860798230e09ad
wenjiu <2784307979@qq.com>
2025-06-07 06:38:00 +0800

    chore: welcome
    Signed-off-by: wenjiu <2784307979@qq.com>

--- option_parser/src/welcome.rs
+++ option_parser/src/welcome.rs
@@ -5,23 +5,27 @@ use owo_colors::OwoColorize;
 use remu_utils::Platform;
 
 const REMU: &str = r#"
-                        ,------. ,------.,--.   ,--.,--. ,--. 
-                        |  .--. '|  .---'|   `.'   ||  | |  | 
-                        |  '--'.'|  `--, |  |'.'|  ||  | |  | 
-                        |  |\  \ |  `---.|  |   |  |'  '-'  ' 
-                        `--' '--'`------'`--'   `--' `-----'              "#;
+,------. ,------.,--.   ,--.,--. ,--. 
+|  .--. '|  .---'|   `.'   ||  | |  | 
+|  '--'.'|  `--, |  |'.'|  ||  | |  | 
+|  |\  \ |  `---.|  |   |  |'  '-'  ' 
+`--' '--'`------'`--'   `--' `-----'  
+    An Computer System Emulator!"#;
 
 pub fn welcome(platform: &Platform) {
-    Logger::show("Welcome to REMU - An Computer System Emulator!", Logger::CONGRATULATIONS);
+    Logger::show("Welcome to", Logger::CONGRATULATIONS);
     
     println!("{}", REMU.fg_rgb::<0x2E, 0x31, 0x92>().bold());
 
     let mut option = Options::default();
     option.font = Fonts::FontSimple;
     option.colors = vec![Colors::Red];
+    option.spaceless = true;
 
     say(Options {
         text: platform.to_string(),
         ..option
     });
+
+    println!();
 }
