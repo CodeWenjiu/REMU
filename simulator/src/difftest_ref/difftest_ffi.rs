96b7294a1993e171a51f82de5428685ca0cc5c03
wenjiu <2784307979@qq.com>
2025-06-08 21:34:31 +0800

    feat: using dlopen2
    less code, less error, less dependecies😎
    
    Signed-off-by: wenjiu <2784307979@qq.com>

--- simulator/src/difftest_ref/difftest_ffi.rs
+++ simulator/src/difftest_ref/difftest_ffi.rs
@@ -12,8 +12,6 @@ use state::reg::{AnyRegfile, RegfileIo};
 
 use super::DifftestRefFfiApi;
 
-include!(concat!("../../bindings.rs"));
-
 #[allow(dead_code)]
 const DIFFTEST_TO_DUT: bool = false;
 #[allow(dead_code)]
@@ -45,42 +43,6 @@ impl From<&AnyRegfile> for DifftestFFICpuState {
     }
 }
 
-impl From<&AnyRegfile> for riscv32_CPU_state {
-    fn from(regfile: &AnyRegfile) -> Self {
-        let mut gpr = [0; 32];
-        for (i, a) in regfile.get_gprs().iter().enumerate() {
-            gpr[i] = *a;
-        }
-        riscv32_CPU_state { gpr, pc: regfile.read_pc() }
-    }
-}
-
-#[test]
-fn difftest_ffi_test() {
-    unsafe {
-        let mut regfile: riscv32_CPU_state = riscv32_CPU_state { gpr: [0; 32], pc: 0x80000000 };
-
-        println!("{:?}", regfile);
-        difftest_init(0);
-        difftest_memcpy(0, std::ptr::null_mut(), 0, DIFFTEST_TO_REF);
-        difftest_regcpy(&mut regfile as *mut _ as *mut std::os::raw::c_void, DIFFTEST_TO_DUT);
-        println!("{:?}", regfile);
-        difftest_exec(0);
-        difftest_raise_intr(0);
-    }
-}
-
-pub fn difftestffi_init(regfile: &AnyRegfile, bin: Vec<u8>, reset_vector: u32) {
-    unsafe {
-        difftest_init(0);
-        
-        let mut regfile = riscv32_CPU_state::from(regfile);
-        difftest_regcpy(&mut regfile as *mut _ as *mut std::os::raw::c_void, DIFFTEST_TO_REF);
-
-        difftest_memcpy(reset_vector, bin.as_ptr() as *mut std::os::raw::c_void, bin.len() as u64, DIFFTEST_TO_REF);
-    }
-}
-
 pub struct FFI {
     container: Container<DifftestFFIApi>
 }
@@ -98,7 +60,7 @@ impl DifftestRefFfiApi for FFI {
         unsafe {
             (self.container.difftest_init)(0);
 
-            let mut regfile = riscv32_CPU_state::from(regfile);
+            let mut regfile = DifftestFFICpuState::from(regfile);
             (self.container.difftest_regcpy)(&mut regfile as *mut _ as *mut std::os::raw::c_void, DIFFTEST_TO_REF);
 
             (self.container.difftest_memcpy)(reset_vector, bin.as_ptr() as *mut std::os::raw::c_void, bin.len() as u64, DIFFTEST_TO_REF);
@@ -107,15 +69,15 @@ impl DifftestRefFfiApi for FFI {
 
     fn step_cycle(&mut self) -> ProcessResult<()> {
         unsafe {
-            difftest_exec(1);
+            (self.container.difftest_exec)(1);
         }
         Ok(())
     }
 
     fn test_reg(&self, dut: &AnyRegfile) -> ProcessResult<()> {
         unsafe {
-            let mut regfile: riscv32_CPU_state = riscv32_CPU_state { gpr: [0; 32], pc: 0x80000000 };
-            difftest_regcpy(&mut regfile as *mut _ as *mut std::os::raw::c_void, DIFFTEST_TO_DUT);
+            let mut regfile: DifftestFFICpuState = DifftestFFICpuState { gpr: [0; 32], pc: 0x80000000 };
+            (self.container.difftest_regcpy)(&mut regfile as *mut _ as *mut std::os::raw::c_void, DIFFTEST_TO_DUT);
             if regfile.pc != dut.read_pc() {
                 log_error!(format!(
                     "Dut PC: {:#010x}, Ref PC: {:#010x}",
@@ -144,7 +106,7 @@ impl DifftestRefFfiApi for FFI {
         for (addr, data) in watchpoint {
             unsafe {
                 let mut buf = [0; 4];
-                difftest_memcpy(addr, buf.as_mut_ptr() as *mut std::os::raw::c_void, 4, DIFFTEST_TO_DUT);
+                (self.container.difftest_memcpy)(addr, buf.as_mut_ptr() as *mut std::os::raw::c_void, 4, DIFFTEST_TO_DUT);
                 let ref_data = u32::from_le_bytes(buf);
                 if ref_data != data {
                     log_error!(format!(
@@ -161,14 +123,14 @@ impl DifftestRefFfiApi for FFI {
 
     fn set_ref(&self, target: &AnyRegfile) {
         unsafe {
-            let mut regfile = riscv32_CPU_state::from(target);
-            difftest_regcpy(&mut regfile as *mut _ as *mut std::os::raw::c_void, DIFFTEST_TO_REF);
+            let mut regfile = DifftestFFICpuState::from(target);
+            (self.container.difftest_regcpy)(&mut regfile as *mut _ as *mut std::os::raw::c_void, DIFFTEST_TO_REF);
         }
     }
 
     fn set_mem(&self, addr:u32, data:Vec<u8>) {
         unsafe {
-            difftest_memcpy(addr, data.as_ptr() as *mut std::os::raw::c_void, data.len() as u64, DIFFTEST_TO_REF);
+            (self.container.difftest_memcpy)(addr, data.as_ptr() as *mut std::os::raw::c_void, data.len() as u64, DIFFTEST_TO_REF);
         }
     }
 }
